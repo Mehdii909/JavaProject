34fb39f18db6cb3be91ec1250f128bf82847f616
Mehdii909 <76838172+Mehdii909@users.noreply.github.com>
2022-07-31 12:12:40 +0100

    add another line

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("add another line");
  	// write your code here
     }
 }
